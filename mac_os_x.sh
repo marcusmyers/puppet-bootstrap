e40875b046ba69ae74a6dad9fc885efa6d17c527
Mark Myers <marcusmyers@gmail.com>
2014-06-14 14:21:43 -0400

    Updated windows and mac scripts to install the latest versions.

--- mac_os_x.sh
+++ mac_os_x.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# This bootstraps Puppet on Mac OS X 10.8 and 10.7.
+# This bootstraps Puppet on Mac OS X 10.7 and above
 #
 # Optional environmental variables:
 #   - FACTER_PACKAGE_URL: The URL to the Facter package to install.
@@ -11,8 +11,9 @@ set -e
 #--------------------------------------------------------------------
 # Modifiable variables, please set them via environmental variables.
 #--------------------------------------------------------------------
-FACTER_PACKAGE_URL=${FACTER_PACKAGE_URL:-"http://downloads.puppetlabs.com/mac/facter-1.7.2.dmg"}
-PUPPET_PACKAGE_URL=${PUPPET_PACKAGE_URL:-"http://puppetlabs.com/downloads/mac/puppet-3.2.3.dmg"}
+FACTER_PACKAGE_URL=${FACTER_PACKAGE_URL:-"http://downloads.puppetlabs.com/mac/facter-2.0.2.dmg"}
+HIERA_PACKAGE_URL=${HIERA_PACKAGE_URL:-"http://downloads.puppetlabs.com/mac/hiera-1.3.4.dmg"}
+PUPPET_PACKAGE_URL=${PUPPET_PACKAGE_URL:-"http://puppetlabs.com/downloads/mac/puppet-3.6.2.dmg"}
 
 #--------------------------------------------------------------------
 # NO TUNABLES BELOW THIS POINT.
@@ -53,6 +54,7 @@ function install_dmg() {
 
 # Install Puppet and Facter
 install_dmg "Puppet" "${PUPPET_PACKAGE_URL}"
+install_dmg "Hiera" "${HIERA_PACKAGE_URL}"
 install_dmg "Facter" "${FACTER_PACKAGE_URL}"
 
 # Hide all users from the loginwindow with uid below 500, which will include the puppet user
